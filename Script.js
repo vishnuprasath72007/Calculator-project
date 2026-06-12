bafaa23efaa3e2aa20172344458ead58bdbce250
vishnuprasath72007 <vishnuprasath72007@gmail.com>
2026-06-12 15:56:03 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,22 @@
+let display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(
+            display.value
+            .replace(/÷/g,'/')
+            .replace(/×/g,'*')
+        );
+    }
+    catch{
+        display.value = "Error";
+    }
+}
